806f79328c0acd9a207ae6cd58163849bc6bede0
ANIKA GOEL <149056289+anikagoell@users.noreply.github.com>
2026-08-27 16:00:48 +0530

    Create resistanceColorBand.py

---
+++ resistanceColorBand.py
@@ -0,0 +1,30 @@
+def color_code(color):
+    code = {
+            "black":0 ,
+            "brown":1,
+            "red":2,
+            "orange":3,
+            "yellow":4,
+            "green":5,
+            "blue":6,
+            "violet":7,
+            "grey":8,
+            "white":9,
+    }
+    return code[color]
+
+
+def colors():
+    l = [
+            "black",
+            "brown",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "violet",
+            "grey",
+            "white",
+    ]
+    return l
